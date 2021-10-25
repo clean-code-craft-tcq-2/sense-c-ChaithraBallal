4adeb7adc83f0cef814b770eebb00bf2bb6c6ae8
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2021-10-25 16:24:44 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,37 @@
+/***********************************************************************
+ *  Stats.c
+ *  Implementation of statistics from a bunch of reading from a sensor
+ ***********************************************************************/
+  
+#include "stdio.h"
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+struct Stats compute_statistics(const float* numberset, int setlength)
+{
+	struct Stats S;
+	int i;
+	S.min = numberset[0];
+	S.max = numberset[0];
+	S.average = 0.0;
+	S.Totalsum = 0.0;
+	
+	for(i=0; i<setlength; i++)
+	{
+		S.Totalsum += numberset[i];
+		
+		if(S.min > numberset[i])
+		{
+			S.min = numberset[i];
+		}
+		
+		if (S.max < numberset[i])
+		{
+			S.max = numberset[i];
+		}
+	}
+	
+	S.average = S.Totalsum/setlength;
+	return S;	
 }
 
 int emailAlertCallCount = 0;
