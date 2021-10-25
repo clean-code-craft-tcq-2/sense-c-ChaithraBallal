78fdefc4fa250aab3049a6ee9c557c0d16f5036a
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2021-10-25 16:25:36 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,18 @@
+/***********************************************************************
+ *  Stats.h
+ *  Implementation of statistics from a bunch of reading from a sensor
+ ***********************************************************************/
 
+#include "stdio.h"
 struct Stats compute_statistics(const float* numberset, int setlength);
+
+typedef struct 
+{
+	float min;             /*minimum of the array*/
+	float max;             /*maximum of the array*/
+	float average;         /*average of the array*/
+	float Totalsum;        /*sum of the array*/
+} Stats;
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
