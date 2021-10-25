c2d16f582e12548615bdd9c33aeeb63fb43ef47e
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2021-10-25 16:57:14 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -6,9 +6,9 @@
 #include "stdio.h"
 #include "stats.h"
 #include "math.h"
-struct Stats_st compute_statistics(const float* numberset, int setlength)
+struct Stats compute_statistics(const float* numberset, int setlength)
 {
-	struct Stats_st S;
+	struct Stats S;
 	int i;
 	S.min = numberset[0];
 	S.max = numberset[0];
@@ -18,19 +18,19 @@ struct Stats_st compute_statistics(const float* numberset, int setlength)
 	for(i=0; i<setlength; i++)
 	{
 		S.Totalsum += numberset[i]; /*Calculating the total sum*/
-		
+		/*Fetch the minimum value*/
 		if(S.min > numberset[i])
 		{
-			S.min = numberset[i];
+		   S.min = numberset[i];
 		}
-		
+		/*Fetch the maximum value*/
 		if (S.max < numberset[i])
 		{
-			S.max = numberset[i];
+	           S.max = numberset[i];
 		}
 	}
-	
-	S.average = S.Totalsum/setlength; /* calculating the average of the elements*/
+	/* calculating the average of the elements*/
+	S.average = S.Totalsum/setlength; 
 	return S;	
 }
 
