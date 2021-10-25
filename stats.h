8613be00c221d0b377d7d1b78c1be5d44efc0b23
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2021-10-25 16:55:13 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,18 +2,19 @@
  *  Stats.h
  *  Implementation of statistics from a bunch of reading from a sensor
  ***********************************************************************/
+#ifndef STATS_H_
+#define STATS_H_
 
 #include "stdio.h"
 #include "math.h"
 
-struct Stats_st compute_statistics(const float* numberset, int setlength);
-
-typedef struct 
+struct Stats
 {
 	float min;             /*minimum of the array*/
 	float max;             /*maximum of the array*/
 	float average;         /*average of the array*/
 	float Totalsum;        /*sum of the array*/
-} Stats_st;
+};
 
+struct Stats compute_statistics(const float* numberset, int setlength);
 
